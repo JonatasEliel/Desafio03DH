09a9a83e8b729fccf4ee45cc75d64f68f9319396
Jonatas Eliel <plipma@hotmail.com>
2020-12-04 11:40:23 -0300

    criando a splash screen

--- app/src/main/java/br/com/digitalhouse/desafio03dh/view/MainActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafio03dh/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.digitalhouse.desafio03dh
+package br.com.digitalhouse.desafio03dh.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.digitalhouse.desafio03dh.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
